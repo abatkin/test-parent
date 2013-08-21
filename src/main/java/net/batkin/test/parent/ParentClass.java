89fe95a01ea1da4e5d429575de4cffd241b6bd28
Adam Batkin <adam@batkin.net>
2013-08-20 20:00:25 -0400

    initial commit

---
+++ src/main/java/net/batkin/test/parent/ParentClass.java
@@ -0,0 +1,9 @@
+package net.batkin.test.parent;
+
+import net.batkin.test.child.ChildClass;
+
+public class ParentClass {
+	public static void main(String[] args) {
+		System.err.println(ChildClass.sayHello("Microsoft Bob"));
+	}
+}
